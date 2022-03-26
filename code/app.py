6c3b5dc575cc5d5c47057b7a936f86957cc50c9c
Adrian Espi <zurich85aep@gmail.com>
2022-03-26 15:47:36 +0100

    version 2

--- code/app.py
+++ code/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, jsonify,send_from_directory, abort
+from flask import Flask, render_template, request, redirect, url_for, jsonify,send_from_directory, abort
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
 import base64
-import time
-from fastapi.responses import FileResponse
+import webbrowser
+from requests import get
 #import pytesseract
 #C:\Program Files\Tesseract-OCR
 #tessdata_dir_config = '--tessdata-dir"C:\\Program Files\\Tesseract-OCR\\tessdata"'
@@ -19,6 +19,9 @@ else:
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS  #Partimos la cadena 1 vez y devuelve una lista con 2 valores
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -29,10 +32,22 @@ def home():
 
 @app.route('/uploader', methods = ['POST', ])
 def upload_file():
-   # NO_VALID_IMAGE = "No se ha proporcionado una imagen valida."
     if request.method == 'POST' and request.files:
         f = request.files['image']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        if f and allowed_file(f.filename.lower()):
+            filename = secure_filename(f.filename)
+            numero = 1
+            ext = filename.rsplit ('.', 1) [1]
+            filename = str(numero) + '.' + "png"   #filename = str(numero) + '.' + str(ext) Asi mantenemos la extension, pero quiero cambiarla
+            """while (os.path.isfile(filename)):   #No lo voy a usar por que cada imagen que cree luego la borro
+                numero += 1 
+                filename = numero + '.' + ext"""
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #return render_template('home.html')
+            #return redirect(url_for("localhost:5050/imagen", filename=filename))
+            return render_template('results.html', text=("Imagen Subida Correctamente", filename, UPLOAD_FOLDER)) , webbrowser.open_new("http:localhost:5050/imagen")
+        return "Archivo no permitido"
+        
         """img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
         text = pytesseract.image_to_string(img)
         try:
@@ -43,20 +58,16 @@ def upload_file():
         if not os.environ.get('DOCKER', '') == "yes":
             myobj = gTTS(text=text, slow=False)
             myobj.save(app.config['UPLOAD_FOLDER'] + "/speech.mp3")
-            playsound(app.config['UPLOAD_FOLDER'] + "/speech.mp3") """
-
-        return render_template('results.html', text=("Imagen Subida Correctamente", UPLOAD_FOLDER))
-
+            playsound(app.config['UPLOAD_FOLDER'] + "/speech.mp3") """        
     return render_template('home.html')
+
 
 @app.route("/download/<filename>", methods=['GET'])
 #@app.route("http://localhost:5000/download/<filename>/download/*.*", methods=['GET'])
 def download(filename):
     if request.method == "GET":
         if os.path.isfile(os.path.join('..\\resultados\\images', filename)):
-            return send_from_directory('..\\resultados\\images', filename, as_attachment=True)
-        abort(404)
-
-
+            return send_from_directory('..\\resultados\\images\\filename', filename, as_attachment=True)
+        
 if __name__ == '__main__':
     app.run(debug = True)
--- code2/app2.py
+++ code2/app2.py
@@ -1,16 +1,20 @@
 from werkzeug.utils import secure_filename
-from PIL import Image
+from PIL import Image, ImageFilter
 import os
 import pytesseract
 import json
 import base64
+from requests import get
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-from fastapi import FastAPI, Form, Request
 from fastapi.responses import PlainTextResponse, HTMLResponse, FileResponse
 from starlette.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import StreamingResponse
+from io import BytesIO
 
 if os.environ.get('DOCKER', '') == "yes":
     DOWNLOAD_FOLDER = 'C:\\Users\\zuric\\Documents\\EntornosPython\\Docker2\\resultados\\images'
@@ -24,18 +28,107 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = FastAPI()
 
 #@app.route("http://localhost:5000/download/<filename>/download/*.*", methods=['GET'])
+"""
+@app.get("/") #'http://localhost:5050/download/2.PNG
+def main():
+    def iterfile():  
+        with open('..\\resultados\\images\\2.PNG', mode="rb") as file_like:  
+            yield from file_like  
+    return StreamingResponse(iterfile(), media_type="image/jpeg")
+
+@app.post("/")
+def image_filter(img: UploadFile = File(...)):
+    original_image = Image.open(img.file)
+    original_image = original_image.filter(ImageFilter.BLUR)
+
+    filtered_image = BytesIO()
+    original_image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+
+    return StreamingResponse(filtered_image, media_type="image/jpeg")
+
+"""
+
+nombreimagen = "1.png"
+imgruta = os.path.join(DOWNLOAD_FOLDER, nombreimagen)
+
+# Jinja2 template instance for returning webpage via template engine
+templates = Jinja2Templates(directory="templates")
+@app.get('/getimagen', response_class=HTMLResponse)
+async def getimagen(request: Request):
+    img = Image.open(os.path.join(imgruta))
+    text = pytesseract.image_to_string(img)
+    result = json.dumps(text)
+    return templates.TemplateResponse("imagen.html", {"request": request, "img": img, "text": text, "result": result})
+
+
+
+
+@app.get("/jsontexto")
+async def jsontexto():
+    #Este seria el json del texto extraido de la imagen:
+    #Convertimos imagen a Texto
+    img = Image.open(os.path.join(imgruta))
+    text = pytesseract.image_to_string(img)
+    result = json.dumps(text)
+    result2 = json.loads(result)
+    return {'json': result}, {'texto': result2}
+
+@app.get('/imagen') # Asi mostramos la imagen leida
+async def imagen():
+    if os.path.exists(imgruta):
+        img = Image.open(os.path.join(imgruta))
+        text = pytesseract.image_to_string(img)
+        result = json.dumps(text)
+        return FileResponse(imgruta)
+    return {"error": "El archivo no existe"}
+
+
+"""
+@app.get('/imagen/<filenane>')
+def imagen(filename):
+    if os.path.exists(filename): 
+        return FileResponse(filename)
+
+    return {"error": "El archivo no existe"}
+"""
+"""
+@app.get('/imagen')
+def imagen():
+    imagen = get("http://localhost:5000/download/3.png")
+    return imagen
+    #return render_template("imagen.html", imagen=imagen)"""
+
+
+
+#print(jsontexto())
+
+#print(img)
+#print(text)
+
+"""@app.post("/jsonimage")
+def jsonimage():
+    #Este seria el json de la imagen:
+    data = {}
+    with open(imgruta, mode='rb') as file:
+        img = file.read()
+    data['img'] = base64.b64encode(img)
+    result = json.dumps(data)
+    return result
+print(jsonimage())"""
+
 
 """@app.get('/')
 def index():
          return {'mensaje': '¡Ha creado el servicio FastApi correctamente! '}"""
 
-"""
-@app.get('/query/{uid}')
+
+"""@app.get('/query/{uid}')
 def query(uid):
         msg = f'El uid que consulta es: {uid}'
-        return {'success': True, 'msg': msg}
+        return {'success': True, 'msg': msg}"""
 
-
+"""
 @app.get("/") #Escucha en la raiz del proyecto (raiz = @app.get("/"))
 def raiz():
     return RedirectResponse(url="/docs/")
@@ -48,24 +141,27 @@ def image_endpoint():
     #text = pytesseract.image_to_string(img)
     return {"mensaje": "Prueba de mensaje"}
 """
-# Jinja2 template instance for returning webpage via template engine
-templates = Jinja2Templates(directory="templates")
 
-@app.get('/get-webpage', response_class=HTMLResponse)
-async def get_webpage(request: Request):
-    return templates.TemplateResponse("results.html", {"request": request, "message": "Contact Us"})
 
 """
-@app.get("/download/")
-async def main():
-    return FileResponse("Hola")"""
+@app.get("/download/{filename}" , response_class=FileResponse)
+async def image_endpoint(filename):
+    #if os.path.isfile(os.path.join('..\\resultados\\images', filename)):
+    img = Image.open(os.path.join(app.config['..\\resultados\\images'], secure_filename(filename)))
+    text = pytesseract.image_to_string(img)
+    print(text)
+    data = {}
+    with open('..\\resultados\\images\\logo.jpg', mode='rb') as file:
+        img = file.read()
+    data['img'] = base64.encodebytes(img).decode('utf-8')
+#print(json.dumps(data))
+    result = json.dumps(data)
+    #return FileResponse('..\\resultados\\images')
+    #return result
+    msg = f'El uid que consulta es: <filename> {filename}'
+    return {'success': True, 'msg': msg}
+ """ 
 
-#Convertimos la imagen a Json
-"""data = {}
-with open('..\\resultados\\images\\logo.jpg', mode='rb') as file:
-    img = file.read()
-data['img'] = base64.encodebytes(img).decode('utf-8')
-#print(json.dumps(data))"""
 
 """@app.get("/")
 async def main():
